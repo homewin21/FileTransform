14e35a6fef7d0622009e6229aeef17ab51c76f66
homewin <nianjs@newland.com.cn>
2020-03-24 15:17:47 +0800

    2020-3-24

--- fileupload/src/main/java/com/homewin/fileupload/util/FileUtils.java
+++ fileupload/src/main/java/com/homewin/fileupload/util/FileUtils.java
@@ -5,22 +5,27 @@ import com.homewin.fileupload.domain.FileObject;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author homewin
  */
 public class FileUtils {
+    /**
+     * 获取对应路径下的所有文件和文件夹
+     *
+     * @param path 路径
+     * @return fileObject
+     */
     public static FileObject getPath(String path){
         List<String> fileList =  new ArrayList<>();
         List<String> foldList = new ArrayList<>();
         File file = new File(path);
-        if (file.listFiles() != null) {
-            for (File temp : file.listFiles()) {
-                if (temp.isFile()) {
-                    fileList.add(temp.getName());
-                } else {
-                    foldList.add(temp.getName());
-                }
+        for (File temp : Objects.requireNonNull(file.listFiles())) {
+            if (temp.isFile()) {
+                fileList.add(temp.getName());
+            } else {
+                foldList.add(temp.getName());
             }
         }
         FileObject fileObject = new FileObject();
